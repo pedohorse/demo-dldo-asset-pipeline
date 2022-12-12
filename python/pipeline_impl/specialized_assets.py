46616e4868888f7ac35bb0848f8e34ff593f98db
pedohorse <xapkohheh@gmail.com>
2022-12-12 01:46:42 +0100

    added lock mapping parameter to create version functions

--- python/pipeline_impl/specialized_assets.py
+++ python/pipeline_impl/specialized_assets.py
@@ -8,7 +8,10 @@ class CacheAsset(Asset):
     """
     this asset represents something with cache, for ex: model, geometry sequence, vdb sequence
     """
-    def create_new_version(self, source: Tuple[str, str], frame_range: Tuple[int, int], is_sim: bool = False, version_id: Optional[VersionType] = None, *, extra_env_requirements: Optional[Dict[str, str]] = None, dependencies: Iterable["AssetVersion"] = ()):
+    def create_new_version(self, source: Tuple[str, str], frame_range: Tuple[int, int], is_sim: bool = False, version_id: Optional[VersionType] = None,
+                           *, extra_env_requirements: Optional[Dict[str, str]] = None,
+                           lock_asset_versions: Dict[str, str] = None,
+                           dependencies: Iterable["AssetVersion"] = ()):
         """
 
         :param source: hip file path, and rop node path that generate final cache
@@ -16,12 +19,16 @@ class CacheAsset(Asset):
         :param frame_range:
         :param is_sim:
         :param extra_env_requirements:
+        :param lock_asset_versions:
         :param dependencies:
         :return:
         """
         base_env_requirements = {'user': getuser()}  # theoretically all packages should be retrieved here from env, but we have a simplified example
         if extra_env_requirements:
             base_env_requirements.update(extra_env_requirements)
+
+        if lock_asset_versions is None:
+            lock_asset_versions = {}
 
         creation_task_parameters = \
             {'name': f'{self.path_id}, ver {version_id}: data creation',
@@ -31,7 +38,8 @@ class CacheAsset(Asset):
                          'hipdriver': source[1],
                          'frames': list(range(frame_range[0], frame_range[1]+1)),
                          'requirements': {'cpu': {'min': 2, 'pref': 32},
-                                          'cmem': {'min': 4, 'pref': 16}}
+                                          'cmem': {'min': 4, 'pref': 16}},
+                         'locked_asset_versions': lock_asset_versions
                          },
              'env': {'name': 'StandardEnvironmentResolver',  # we assume a single environment resolver is defined for the whole pipeline
                      'attribs': base_env_requirements
@@ -61,10 +69,17 @@ class RenderAsset(Asset):
     """
     this asset represents something rendered, image sequence
     """
-    def create_new_version(self, source: Tuple[str, str], frame_range: Tuple[int, int], version_id: Optional[VersionType] = None, *, extra_env_requirements: Optional[Dict[str, str]] = None, dependencies: Iterable["AssetVersion"] = ()):
+    def create_new_version(self, source: Tuple[str, str], frame_range: Tuple[int, int], version_id: Optional[VersionType] = None,
+                           *, extra_env_requirements: Optional[Dict[str, str]] = None,
+                           lock_asset_versions: Dict[str, str],
+                           dependencies: Iterable["AssetVersion"] = ()):
+
         base_env_requirements = {'user': getuser()}  # theoretically all packages should be retrieved here from env, but we have a simplified example
         if extra_env_requirements:
             base_env_requirements.update(extra_env_requirements)
+
+        if lock_asset_versions is None:
+            lock_asset_versions = {}
 
         creation_task_parameters = \
             {'name': f'{self.path_id}, ver {version_id}: data creation',
@@ -73,8 +88,9 @@ class RenderAsset(Asset):
                          'hipfile': source[0],
                          'hipdriver': source[1],
                          'frames': list(range(frame_range[0], frame_range[1] + 1)),
-                         'requirements': {'cpu': {'min': 2, 'pref':32},
-                                          'cmem': {'min': 4, 'pref': 16}}
+                         'requirements': {'cpu': {'min': 2, 'pref': 32},
+                                          'cmem': {'min': 4, 'pref': 16}},
+                         'locked_asset_versions': lock_asset_versions
                          },
              'env': {'name': 'StandardEnvironmentResolver',  # we assume a single environment resolver is defined for the whole pipeline
                      'attribs': base_env_requirements
