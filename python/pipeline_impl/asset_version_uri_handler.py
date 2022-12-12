c6612e5b857c428bbbca5c85a149fa2bd390d21a
pedohorse <xapkohheh@gmail.com>
2022-12-12 01:46:10 +0100

    implemented is_dynamic, looking for locks in env

--- python/pipeline_impl/asset_version_uri_handler.py
+++ python/pipeline_impl/asset_version_uri_handler.py
@@ -1,3 +1,5 @@
+import os
+import json
 from pipeline.uri_handler import UriHandlerBase
 from pipeline.uri import Uri
 from pipeline.asset import Asset, AssetVersion
@@ -14,10 +16,23 @@ class AssetVersionUriHandler(UriHandlerBase):
     def accepts(self, uri: Uri) -> bool:
         return uri.protocol == 'assetver'
 
+    def _get_lock_mapped_pathid(self, uri: Uri) -> Union[str, None]:
+        """
+        check environment if there is a lock for a given dynamic uri
+        None if there's no lock
+        """
+        if not self.is_dynamic(uri):
+            return None
+        mapping = json.loads(os.environ.get('LBATTR_locked_asset_versions', '{}'))
+        return mapping.get(str(uri))
+
     def fetch(self, uri: Uri) -> Union[AssetVersion, str]:
+        locked_pathid = self._get_lock_mapped_pathid(uri)
         try:
-            assver = self.__director.get_asset_version(uri.path)
+            assver = self.__director.get_asset_version(locked_pathid or uri.path)
         except NotFoundError:
+            if locked_pathid:  # if locked - we do NOT try to resolve dynamically
+                raise
             # maybe uri path is an asset path, then we bring the latest version
             ass = self.__director.get_asset(uri.path)
             assver = ass.get_latest_version()
@@ -26,3 +41,19 @@ class AssetVersionUriHandler(UriHandlerBase):
                 return ''
             return getattr(assver, uri.query)
         return assver
+
+    def is_dynamic(self, uri: Uri) -> bool:
+        """
+        dynamic asset version is one without
+        here we do NOT want to consider uri not dynamic if there is a lock for it
+            cuz that can cause next publisher to not lock such uri
+
+        :param uri:
+        :return:
+        """
+        try:
+            assver = self.__director.get_asset_version(uri.path)
+            return False
+        except NotFoundError:  # cuz it's not a version - it must be an asset
+            ass = self.__director.get_asset(uri.path)
+            return True
