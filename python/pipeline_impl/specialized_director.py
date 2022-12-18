417e282c78465a02490a0e42e89959d5cf8ab245
pedohorse <xapkohheh@gmail.com>
2022-12-19 00:18:15 +0100

    AssetFactory implementation

--- python/pipeline_impl/specialized_director.py
+++ python/pipeline_impl/specialized_director.py
@@ -1,5 +1,20 @@
-from pipeline.director import Director
+from pipeline.director import Director, AssetFactory
+from pipeline.specialized_asset_base import SpecializedAssetBase, Asset
 from .specialized_assets import CacheAsset, RenderAsset
+
+from typing import Type
+
+
+class SpecializedAssetFactory(AssetFactory):
+    def __init__(self, specialized_class: Type[SpecializedAssetBase], director: Director):
+        self.__director = director
+        self.__class = specialized_class
+
+    def __call__(self, asset_path_id: str) -> Asset:
+        return self.__class(asset_path_id, self.__director)
+
+    def asset_type(self) -> Type[Asset]:
+        return self.__class
 
 
 class PipelineDirector(Director):
