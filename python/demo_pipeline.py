61ef93a01f8c12a7f33aa8b25a6bf374f23482ba
pedohorse <xapkohheh@gmail.com>
2023-01-15 02:41:04 +0100

    removed special asset registration, added lifeblood task scheduler construction

--- python/demo_pipeline.py
+++ python/demo_pipeline.py
@@ -2,20 +2,19 @@ import os
 from pipeline_impl.specialized_director import SpecializedAssetFactory, PipelineDirector, Director
 from pipeline.data_access_interface import NotFoundError, TaskSchedulerNotAvailable  # export
 from pipeline_impl.sqlite_data_manager import SqliteDataManagerWithLifeblood
+from pipeline_impl.lifeblood_task_scheduler import LifebloodDataScheduler
 from pipeline_impl.asset_uri_handler import AssetUriHandler
 from pipeline_impl.asset_version_uri_handler import AssetVersionUriHandler
 
 from pipeline_impl.specialized_assets import CacheAsset, RenderAsset, ComposeAsset
 
 lb_addr = ('192.168.0.28', 1384)
-__dm = SqliteDataManagerWithLifeblood(os.path.join(os.environ['PIPELINE_ROOT'], 'smth.db'), lb_addr)
+__scheduler = LifebloodDataScheduler(lb_addr)
+__dm = SqliteDataManagerWithLifeblood(os.path.join(os.environ['PIPELINE_ROOT'], 'smth.db'), __scheduler)
+__scheduler.add_task_completion_callback_receiver(__dm)
 __director: PipelineDirector = PipelineDirector(__dm)
 __director.register_uri_handler(AssetUriHandler(__director))
 __director.register_uri_handler(AssetVersionUriHandler(__director))
-
-__director.register_asset_type(SpecializedAssetFactory(CacheAsset, __director))
-__director.register_asset_type(SpecializedAssetFactory(RenderAsset, __director))
-__director.register_asset_type(SpecializedAssetFactory(ComposeAsset, __director))
 
 
 def get_director() -> PipelineDirector:
